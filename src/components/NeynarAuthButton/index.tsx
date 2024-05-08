fb20b47af906bc37c9e54f6fb31457fddb7fdb18
Shreyaschorge <shreyas@neynar.com>
2024-05-08 12:58:55 +0530

    Take in all the button props for NeynarAuthButton

--- src/components/NeynarAuthButton/index.tsx
+++ src/components/NeynarAuthButton/index.tsx
@@ -2,39 +2,36 @@ import React from "react";
 import { styled } from "@pigment-css/react";
 import PlanetBlackIcon from "./icons/PlanetBlackIcon";
 
-interface ButtonProps {
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   label?: string;
-  onClick?: () => void;
-  children?: React.ReactNode;
 }
 
-// color: ${(props) => props.theme.vars.palette.primary};
-
-const Button = styled.button`
-  background-color: #ffffff;
-  border: none;
-  color: ${(props) => props.theme.vars.palette.primary};
-  padding: 15px;
-  font-size: 15px;
-  font-weight: 600;
-  line-height: 18.9px;
-  border-radius: 100px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  gap: 10px;
-  cursor: pointer;
-  text-decoration: none;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
-  transition: background-color 0.3s;
-`;
+const Button = styled.button<ButtonProps>((props) => ({
+  backgroundColor: "#ffffff",
+  border: "none",
+  color: "#000000",
+  padding: "15px",
+  fontSize: "15px",
+  fontWeight: "600",
+  lineHeight: "18.9px",
+  borderRadius: "100px",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  gap: "10px",
+  cursor: "pointer",
+  textDecoration: "none",
+  boxShadow: "0 2px 4px rgba(0, 0, 0, 0.2)",
+  transition: "background-color 0.3s",
+}));
 
 export const NeynarAuthButton: React.FC<ButtonProps> = ({
   label = "Sign in with Neynar",
-  onClick,
+  children,
+  ...rest
 }) => (
-  <Button onClick={onClick}>
+  <Button {...rest}>
     <PlanetBlackIcon />
-    <span>{label}</span>
+    {label}
   </Button>
 );
