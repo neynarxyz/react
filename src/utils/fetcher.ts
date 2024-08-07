684a763299a84f9376da7f8fafc494c055abc7f9
dylsteck <dylancsteck@gmail.com>
2024-08-07 19:16:40 -0400

    fix: remove `x-frontend-sdk` then add `x-sdk` and `x-sdk-version`

--- src/utils/fetcher.ts
+++ src/utils/fetcher.ts
@@ -3,7 +3,8 @@ const SDK_VERSION = process.env.SDK_VERSION || '0.0.0';
 const customFetch = async (url: string, options: RequestInit = {}): Promise<Response> => {
   options.headers = {
     ...options.headers,
-    'x-frontend-sdk-version': SDK_VERSION,
+    'x-sdk': 'react',
+    'x-sdk-version': SDK_VERSION,
   };
 
   return fetch(url, options);
