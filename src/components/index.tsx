66660c0c142f419d42518203e0810fc6c90fd613
dylsteck <dylancsteck@gmail.com>
2024-08-08 14:59:58 -0400

    fix: ensure all main organisms are properly exported from `@neynar/react`

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,3 +1,6 @@
-export { NeynarProfileCard } from "./organisms/NeynarProfileCard";
-export { NeynarCastCard } from "./organisms/NeynarCastCard";
 export { NeynarAuthButton } from "./organisms/NeynarAuthButton";
+export { NeynarCastCard } from "./organisms/NeynarCastCard"; 
+export { NeynarConversationList } from "./organisms/NeynarConversationList"; 
+export { NeynarFeedList } from "./organisms/NeynarFeedList"; 
+export { NeynarProfileCard } from "./organisms/NeynarProfileCard";
+
