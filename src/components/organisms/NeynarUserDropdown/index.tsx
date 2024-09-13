181008ac5e4bbfaf7cd929ac16542cfaa69d711c
Kevin O'Connell <kevinoconnell42@gmail.com>
2024-09-13 17:55:51 -0400

    add styling and use styled

--- src/components/organisms/NeynarUserDropdown/index.tsx
+++ src/components/organisms/NeynarUserDropdown/index.tsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect, useRef } from 'react';
+import { styled } from '@pigment-css/react';
 import { NEYNAR_API_URL } from '../../../constants';
 import customFetch from '../../../utils/fetcher';
 import { useNeynarContext } from '../../../contexts';
+import UserDropdown from '../../molecules/UserDropdown';
 
 interface User {
   fid: number;
@@ -11,13 +13,28 @@ interface User {
 }
 
 export interface NeynarUserDropdownProps {
-    value?: string;
-    onChange?: (value: string) => void;
+    value: string;
+    onChange: (value: string) => void;
     style?: React.CSSProperties;
     placeholder?: string;
     disabled?: boolean;
     viewerFid?: number;
+    customStyles?: {
+        dropdown?: React.CSSProperties;
+        listItem?: React.CSSProperties;
+        avatar?: React.CSSProperties;
+        userInfo?: React.CSSProperties;
+        };
 }
+
+const Container = styled.div(() => ({
+  position: 'relative',
+}));
+
+const Input = styled.input(() => ({
+  width: '100%',
+  padding: '8px',
+}));
 
 async function fetchUsersByQuery({
   q,
@@ -40,133 +57,91 @@ async function fetchUsersByQuery({
 }
 
 export const NeynarUserDropdown: React.FC<NeynarUserDropdownProps> = ({
- value: propValue,
- onChange,
+  value,
+  onChange,
   style = {},
   placeholder = 'Enter FIDs or usernames',
   disabled = false,
-  viewerFid
+  viewerFid,
+customStyles = {},
 }) => {
-    const { client_id } = useNeynarContext();
-    const [internalValue, setInternalValue] = useState(propValue || '');
-    const [currentValue, setCurrentValue] = useState<string>('');
-    const [showDropdown, setShowDropdown] = useState<boolean>(false);
-    const [users, setUsers] = useState<User[]>([]);
-    const inputRef = useRef<HTMLInputElement>(null);
-  
-    useEffect(() => {
-      if (propValue !== undefined) {
-        setInternalValue(propValue);
-      }
-    }, [propValue]);
+  const { client_id } = useNeynarContext();
+  const [currentValue, setCurrentValue] = useState<string>('');
+  const [showDropdown, setShowDropdown] = useState<boolean>(false);
+  const [users, setUsers] = useState<User[]>([]);
+  const inputRef = useRef<HTMLInputElement>(null);
 
-    useEffect(() => {
-      const values = internalValue.split(',');
-      if (!values[values.length -1]) {
-       setCurrentValue('');
-        return;
-      }
-      setCurrentValue(values[values.length - 1].trim());
-    }, [internalValue]);
-  
-    useEffect(() => {
-        const shouldFetchUsers = currentValue !== '' && !/^\d+$/.test(currentValue);
-        if (shouldFetchUsers) {
-          fetchUsers(currentValue);
-        } else {
-          setShowDropdown(false);
-        }
-      }, [currentValue]);
-    
-    const fetchUsers = async (query: string) => {
-      const fetchedUsers = await fetchUsersByQuery({ q: query, viewerFid, client_id });
-      if (fetchedUsers) {
-        setUsers(fetchedUsers);
-        setShowDropdown(true);
-      }
-    };
-  
-    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-      const newValue = e.target.value;
-      setInternalValue(newValue);
-      onChange && onChange(newValue);
-    };
-  
-    const handleUserSelect = (user: User) => {
-      let values = internalValue.split(',');
-      values[values.length - 1] = user.fid.toString();
-      const newValue = values.join(',');
-      setInternalValue(newValue);
-      onChange && onChange(newValue);
+  useEffect(() => {
+    const values = value?.split(',') || [];
+    if (!values[values.length -1]) {
       setCurrentValue('');
+      return;
+    }
+    setCurrentValue(values[values.length - 1].trim());
+  }, [value]);
+
+  useEffect(() => {
+    const shouldFetchUsers = currentValue !== '' && !/^\d+$/.test(currentValue);
+    if (shouldFetchUsers) {
+      fetchUsers(currentValue);
+    } else {
       setShowDropdown(false);
-    };
-  
-    const handleInputFocus = () => {
-      if (currentValue && isNaN(Number(currentValue))) {
-        setShowDropdown(true);
-      }
-    };
-  
-    const handleInputBlur = () => {
-      setTimeout(() => setShowDropdown(false), 200);
-    };
-  
-    return (
-      <div style={{ position: 'relative', ...style }}>
-        <input
-          ref={inputRef}
-          type="text"
-          value={internalValue}
-          onChange={handleInputChange}
-          onFocus={handleInputFocus}
-          onBlur={handleInputBlur}
-          placeholder={placeholder}
-          disabled={disabled}
-          style={{ width: '100%', padding: '8px' }}
-        />
-        {showDropdown && (
-          <ul style={{
-            position: 'absolute',
-            top: '100%',
-            left: 0,
-            width: '100%',
-            maxHeight: '200px',
-            overflowY: 'auto',
-            border: '1px solid #ccc',
-            borderTop: 'none',
-            listStyle: 'none',
-            padding: 0,
-            margin: 0,
-            backgroundColor: 'white',
-            zIndex: 1000
-          }}>
-            {users.map((user) => (
-              <li
-                key={user.fid}
-                onClick={() => handleUserSelect(user)}
-                style={{
-                  padding: '8px',
-                  cursor: 'pointer',
-                  display: 'flex',
-                  alignItems: 'center'
-                }}
-              >
-                <img
-                  src={user.pfp_url}
-                  alt={user.username}
-                  style={{ width: '30px', height: '30px', borderRadius: '50%', marginRight: '10px' }}
-                />
-                <div>
-                  <div>{user.display_name}</div>
-                  <div style={{ fontSize: '0.8em', color: '#666' }}>@{user.username}</div>
-                </div>
-              </li>
-            ))}
-          </ul>
-        )}
-      </div>
-    );
+    }
+  }, [currentValue]);
+
+  const fetchUsers = async (query: string) => {
+    const fetchedUsers = await fetchUsersByQuery({ q: query, viewerFid, client_id });
+    if (fetchedUsers) {
+      setUsers(fetchedUsers);
+      setShowDropdown(true);
+    }
   };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = e.target.value;
+    onChange(newValue);
+  };
+
+  const handleUserSelect = (user: User) => {
+    let values = value.split(',');
+    values[values.length - 1] = user.fid.toString();
+    const newValue = values.join(',');
+    onChange(newValue);
+    setCurrentValue('');
+    setShowDropdown(false);
+  };
+
+  const handleInputFocus = () => {
+    if (currentValue && isNaN(Number(currentValue))) {
+      setShowDropdown(true);
+    }
+  };
+
+  const handleInputBlur = () => {
+    setTimeout(() => setShowDropdown(false), 200);
+  };
+
+  return (
+    <Container style={style}>
+      <Input
+        ref={inputRef}
+        type="text"
+        value={value}
+        onChange={handleInputChange}
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
+        placeholder={placeholder}
+        disabled={disabled}
+      />
+      {showDropdown && (
+        <UserDropdown
+          users={users}
+          onSelect={handleUserSelect}
+          customStyles={customStyles}
+        />
+      )}
+    </Container>
+  );
+};
 
 export default NeynarUserDropdown;
